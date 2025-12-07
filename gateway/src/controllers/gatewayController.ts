af57115db052d7f650002c84116d886ec3566064
fonseca-plx <pedrolucasxf@gmail.com>
2025-12-06 21:42:01 -0300

    feat: improve user and quiz data extraction in GatewayController methods

--- gateway/src/controllers/gatewayController.ts
+++ gateway/src/controllers/gatewayController.ts
@@ -7,7 +7,7 @@ export default class GatewayController {
   static async createUser(req: Request, res: Response) {
     try {
       const data = await restProxy.createUser(req.body);
-      const user = data.user ?? data;
+      const user = data.user?.user ?? data.user ?? data;
       res.status(201).json({ user, _links: makeUserLinks(user) });
     } catch (err: any) {
       res.status(err.response?.status || 500).json({ error: err.message });
@@ -17,7 +17,15 @@ export default class GatewayController {
   static async listUsers(req: Request, res: Response) {
     try {
       const data = await restProxy.listUsers();
-      const users = Array.isArray(data) ? data : data.users ?? data;
+      let users: any[];
+      
+      if (Array.isArray(data)) {
+        users = data.map((item: any) => item.user?.user ?? item.user ?? item);
+      } else {
+        const usersList = data.users ?? data;
+        users = usersList.map((item: any) => item.user?.user ?? item.user ?? item);
+      }
+
       res.json(users.map((user: any) => ({ user, _links: makeUserLinks(user) })));
     } catch (err: any) {
       res.status(500).json({ error: err.message });
@@ -28,7 +36,7 @@ export default class GatewayController {
     try {
       const id = req.params.id as string;
       const data = await restProxy.getUser(id);
-      const user = data.user ?? data;
+      const user = data.user?.user ?? data.user ?? data;
       res.json({ user, _links: makeUserLinks(user) });
     } catch (err: any) {
       res.status(err.response?.status || 500).json({ error: err.message });
@@ -38,7 +46,7 @@ export default class GatewayController {
   static async createQuiz(req: Request, res: Response) {
     try {
       const data = await restProxy.createQuiz(req.body);
-      const quiz = data.quiz ?? data;
+      const quiz = data.quiz?.quiz ?? data.quiz ?? data;
       res.status(201).json({ quiz, _links: makeQuizLinks(quiz) });
     } catch (err: any) {
       res.status(err.response?.status || 500).json({ error: err.message });
@@ -48,7 +56,16 @@ export default class GatewayController {
   static async listQuizzes(req: Request, res: Response) {
     try {
       const data = await restProxy.listQuizzes();
-      const quizzes = Array.isArray(data) ? data : data.quizzes ?? data;
+
+      let quizzes: any[];
+      
+      if (Array.isArray(data)) {
+        quizzes = data.map((item: any) => item.quiz?.quiz ?? item.quiz ?? item);
+      } else {
+        const quizzesList = data.quizzes ?? data;
+        quizzes = quizzesList.map((item: any) => item.quiz?.quiz ?? item.quiz ?? item);
+      }
+
       res.json(quizzes.map((quiz: any) => ({ quiz, _links: makeQuizLinks(quiz) })));
     } catch (err: any) {
       res.status(500).json({ error: err.message });
@@ -59,7 +76,7 @@ export default class GatewayController {
     try {
       const id = req.params.id as string;
       const data = await restProxy.getQuiz(id);
-      const quiz = data.quiz ?? data;
+      const quiz = data.quiz?.quiz ?? data.quiz ?? data;
       res.json({ quiz, _links: makeQuizLinks(quiz) });
     } catch (err: any) {
       res.status(err.response?.status || 500).json({ error: err.message });
@@ -69,7 +86,7 @@ export default class GatewayController {
   static async createRoom(req: Request, res: Response) {
     try {
       const data = await restProxy.createRoom(req.body);
-      const room = data.room ?? data;
+      const room = data.room?.room ?? data.room ?? data;
       const wsUrl = process.env.WS_URL || "http://localhost:4000";
 
       res.status(201).json({ 
@@ -84,7 +101,16 @@ export default class GatewayController {
   static async listRooms(req: Request, res: Response) {
     try {
       const data = await restProxy.listRooms();
-      const rooms = Array.isArray(data) ? data : data.rooms ?? data;
+
+      let rooms: any[];
+      
+      if (Array.isArray(data)) {
+        rooms = data.map((item: any) => item.room?.room ?? item.room ?? item);
+      } else {
+        const roomsList = data.rooms ?? data;
+        rooms = roomsList.map((item: any) => item.room?.room ?? item.room ?? item);
+      }
+
       const wsUrl = process.env.WS_URL || "http://localhost:4000";
 
       res.json(
@@ -102,7 +128,7 @@ export default class GatewayController {
     try {
       const id = req.params.id as string;
       const data = await restProxy.getRoomById(id);
-      const room = data.room ?? data;
+      const room = data.room?.room ?? data.room ?? data;
       const wsUrl = process.env.WS_URL || "http://localhost:4000";
 
       res.json({ 
@@ -118,7 +144,7 @@ export default class GatewayController {
     try {
       const code = req.params.code as string;
       const data = await restProxy.getRoomByCode(code);
-      const room = data.room ?? data;
+      const room = data.room?.room ?? data.room ?? data;
       const wsUrl = process.env.WS_URL || "http://localhost:4000";
 
       res.json({ 
